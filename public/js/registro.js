f44a2205d2bdf7a9e9ba9cc86ab900f6cbf48324
Jose <blancohernandezjoseandres@gmail.com>
2025-05-07 22:28:46 -0400

    guardar datos

--- public/js/registro.js
+++ public/js/registro.js
@@ -17,17 +17,25 @@ document.getElementById('register-form').addEventListener('submit', async (e) =>
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
+                //significa que el contenido que se va a enviar es en formato json
+                // y el servidor lo va a entender como tal
             },
             body: JSON.stringify({ email, password }),
+            //convierte el objeto en una cadena JSON para enviarlo al servidor
         });
 
+
         const data = await response.json();
+        //convierte la respuesta del servidor en un objeto JSON
+        //y lo guarda en la variable data
         
-        // if (!response.ok){
-        //     alert(data.message || 'Error en el registro');
-        //     console.error('Error:', data.message || 'Error en el registro');
-        //     return;
-        // }
+        if (!response.ok){
+            alert(data.message || 'Error en el registro');
+            //si la respuesta no es ok, significa que hubo un error en el registro
+            //y se muestra un mensaje de error
+            console.error('Error:', data.message || 'Error en el registro');
+            return;
+        }
         if (response.ok) {
             alert(data.message);
             window.location.href = 'logint.html'; // Redirigir al login después del registro exitoso
--- public/js/tienda.js
+++ public/js/tienda.js
@@ -10,17 +10,24 @@ if (!token) {
     fetch('http://localhost:3000/api/tienda', {
         method: 'GET',
         headers: {
-            'Authorization': token,  
+            'Authorization': token, 
+            // Enviar el token en la cabecera de autorización
+            // El servidor lo espera en la cabecera Authorization 
         },
     })
     .then(response => {
+        //
         if (!response.ok) {
             if (response.status === 401 || response.status === 403) {
+                // Si la respuesta no es ok y el estado es 401 o 403, redirigir al login
                 alert('Sesión expirada o no autorizada. Redirigiendo al login.');
                 localStorage.removeItem('token');
                 window.location.href = '/public/login.html';
             }
             throw new Error('Error de autenticación');
+            // Si la respuesta no es ok, lanzar un error
+            //y no se ejecuta el siguiente then
+            //y se va al catch
         }
         return response.json();
     })
